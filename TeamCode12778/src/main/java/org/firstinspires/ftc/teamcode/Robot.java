d0e39ffad272eed17d9ab0951f4aebb1ee9cec90
parnika <parnika.nandola@gmail.com>
2025-11-03 19:08:43 -0800

    Initial team code.

--- TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -45,6 +45,8 @@ public class Robot {
     rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
     leftLauncher.setDirection(DcMotor.Direction.REVERSE);
     rightLauncher.setDirection(DcMotor.Direction.FORWARD);
+    transportLeft.setDirection(CRServo.Direction.REVERSE);
+    transportRight.setDirection(CRServo.Direction.REVERSE);
   }
 
   public void setRunUsingEncoder() {
