ed8536c362f5918ff5234b8ceec6f0a4233af3ac
spiderman6767 <emmamnacher@gmail.com>
2025-11-08 11:08:07 -0800

    Update TeleOp2Controllers.java

--- TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2Controllers.java
+++ TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2Controllers.java
@@ -64,9 +64,9 @@ public class TeleOp2Controllers  extends LinearOpMode {
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             // Note: pushing stick forward gives negative value
             double Drive_Speed = 1;
-            lateral = -gamepad1.right_stick_y;
-            axial = gamepad1.right_stick_x;
-            yaw = -gamepad1.left_stick_x;
+            axial = gamepad1.right_stick_y;
+            lateral = gamepad1.right_stick_x;
+            yaw = gamepad1.left_stick_x;
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
             leftFrontPower = axial + lateral + yaw;
