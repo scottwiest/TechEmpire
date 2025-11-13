33011090cf7bbb97e12efa82e3a0986b7caa7934
parnika <parnika.nandola@gmail.com>
2025-11-12 19:18:53 -0800

    Removing else case for setting launch power off

--- TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2Controllers.java
+++ TeamCode12778/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2Controllers.java
@@ -110,7 +110,6 @@ public class TeleOp2Controllers  extends LinearOpMode {
             if (gamepad2.dpadDownWasPressed()) {
                 robot.intake.setPower(1);
                 robot.setTransportPower(1);
-                
             }
             if (gamepad2.dpadDownWasReleased()) {
                 robot.intake.setPower(0);
@@ -133,14 +132,15 @@ public class TeleOp2Controllers  extends LinearOpMode {
                 robot.transportBottom.setPower(-0.5);
                 robot.setLauncherPower(-0.2);
             }
+            if (gamepad2.rightBumperWasReleased()) {
+                robot.transportTop.setPower(0);
+                robot.transportBottom.setPower(0);
+                robot.setLauncherPower(0);
+            }
             if (gamepad1.right_trigger > 0.5){
                 robot.transportTop.setPower(-1);
                 robot.transportBottom.setPower(-1);
                 robot.setLauncherPower(-0.25);
-            }else {
-                robot.transportTop.setPower(0);
-                robot.transportBottom.setPower(0);
-                robot.setLauncherPower(0);
             }
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime);
